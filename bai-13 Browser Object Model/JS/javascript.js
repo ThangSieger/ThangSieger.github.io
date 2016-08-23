760f841e52be7092ab761001714ebcf464a676ca
Vu Viet Thang <toanthang2805@yahoo.com>
2016-08-23 12:10:54 +0700

    bai-13 BOM
    Chua xong

--- bai-13 Browser Object Model/JS/javascript.js
+++ bai-13 Browser Object Model/JS/javascript.js
@@ -1,4 +1,5 @@
-var pos = 0, test, test_status, question choice, choices, chA, chB, chC, correct = 0;
+var pos =0;
+var test, test_status, question_choice, choice, chA, chB, chC, correct = 0;
 var questions = [
 	["What is 10 + 4?", "12", "14", "16", "B"],
 	["What is 20 - 9?", "7", "13", "11", "C"],
